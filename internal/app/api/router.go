e89203646a20d6377b7d692eef28196d5d2146dc
x893675 <x893675@163.com>
2019-10-19 16:23:51 +0800

    :construction: coding for api server

--- internal/app/api/router.go
+++ internal/app/api/router.go
@@ -6,7 +6,7 @@ func (api *ApiApplication) RegisterRouter(app *gin.Engine) {
 	g := app.Group("/api")
 	v1 := g.Group("/v1")
 	{
-		v1.GET("/v1/greeter", api.LoginCtl.Anything)
-		v1.GET("/v1/greeter/:name", api.LoginCtl.Hello)
+		v1.GET("/greeter", api.LoginCtl.Anything)
+		v1.GET("/greeter/:name", api.LoginCtl.Hello)
 	}
 }
