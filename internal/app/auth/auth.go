c67c576a416d86506185f6ecb084ad242b59d449
x893675 <x893675@163.com>
2019-11-04 16:35:52 +0800

    :construction: coding for usersrv

--- internal/app/auth/auth.go
+++ internal/app/auth/auth.go
@@ -115,6 +115,14 @@ func (a *AuthServer) Verify(ctx context.Context, req *auth.LoginRequest, rsp *au
 
 	userid := response.Data[0].RecordID
 	log.Println("in verify, password is ", response.Data[0].Password)
+
+	item := response.Data[0]
+	if item.Password != util.SHA1HashString(req.Password) {
+		return errors.ErrInvalidPassword
+	} else if item.Status != 1 {
+		return errors.ErrUserDisable
+	}
+
 	err = a.generateToken(ctx, userid, rsp)
 	if err != nil {
 		fmt.Println(err.Error())
